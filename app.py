3e40e82545509013937171f8d054bb5b5332a615
mustafa-akgul <akgulmustafatalha79@gmail.com>
2025-09-06 15:40:29 +0300

    fixed app.py

--- app.py
+++ app.py
@@ -10,8 +10,22 @@ app = FastAPI()
 pipeline = CNNLSTMPipeline()
 pipeline.load_pipeline("pipeline.pkl")
 
+
 @app.post("/predict")
 def predict(input_data: InputData):
-    df = pd.DataFrame(input_data.data)[['x','y','z']]
-    predictions = pipeline.predict(df)
-    return {"prediction": predictions.tolist()}
+    try:
+        df = pd.DataFrame(input_data.data)[['x','y','z']]
+
+        if len(df) < pipeline.window_size:
+            return{
+                "error": f"Min {pipeline.window_size} line need, now, there are {len(df)} line. ",
+                "prediction": []
+            }
+        predictions = pipeline.predict(df)
+        return {"prediction": predictions.tolist()}
+    
+    except Exception as e:
+        return {"error": str(e), "received_data": input_data.data}
+
+        
+        
